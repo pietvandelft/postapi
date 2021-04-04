baa666b62ab714305b7df16548603c91c394162f
pietvandelft <hallo@pietvandelft.nl>
2021-04-04 16:57:42 +0200

    docker stuff

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import got from 'got';
 import haversine from 'haversine';
 
 const app: Application = express();
-const PORT: number = 3000;
+const PORT = process.env.PORT || 3000;
 const apiAdamUrl = "https://api.data.amsterdam.nl/atlas/search/adres/?q=";
 
 // interface for the part of the JSON result we use from api.data.amsterdam.nl
@@ -12,11 +12,11 @@ export interface adamPlaceData {
     results: Array<any>
 }
 
+// Start server
 app.listen(
     PORT,
     () => console.log(`running on ${PORT}`)
 )
-
 app.use(express.json());
 
 /**
@@ -26,8 +26,8 @@ app.use(express.json());
  */
 app.get('/distance', (req: Request, res: Response) => {
     (async () => {
-        const address1: string = req.query.address1 as string;
-        const address2: string = req.query.address2 as string;
+        let address1: string = req.query.address1 as string;
+        let address2: string = req.query.address2 as string;
 
         // Check if both inputs are present.
         // We don't do any other validation, as data is not stored.
@@ -55,13 +55,12 @@ app.get('/distance', (req: Request, res: Response) => {
             let distance: number = haversine(values[0], values[1], {unit: 'meter', format: '[lat,lon]'});
 
             res.status(200).send({
-                latLongX: values[0],
-                latLongY: values[1],
-                distanceMeters: distance
+                latLongX: values[0], // lat-long is returned as bonus-data
+                latLongY: values[1], // lat-long is returned as bonus-data
+                distanceMeters: distance // distance in meters (float)
             })
         })
         .catch(error => {
-            console.log(error);
             res.status(404).send({
                 status: "404",
                 error: "Address not found",
@@ -83,15 +82,17 @@ async function getLatLong (address: string){
             else if (apiResult.count > 1){
                 /** If a list of multiple addresses is returned, there is a chance
                     that these are different appartments at the same location.
-                    (e.g. Sesamestreet 34-h, Sesamestreet 34-1)
-                    In this case all the addresses will have the same lat-long,
-                    we check for this and return this lat-long.
-                    Otherwise an error is returned. 
+                    (e.g. Sesame street 34-h, Sesame street 34-1)
+                    In this case all the addresses will have the same lat-long.
+                    We check for this and return this lat-long, otherwise an error is returned.
+
+                    Also, the API returns house number 100 when we search for 10.
+                    For better results we would have to filter these out as wel (not done at the moment).
                 */
                 for (let i = 1; i < apiResult.results.length; i++){
                     if (apiResult.results[i].centroid[0] != apiResult.results[0].centroid[0] ||
                         apiResult.results[i].centroid[1] != apiResult.results[0].centroid[1]){
-                            // We've found a lat-long that is different from the others.
+                            // We've found a lat-long that is different from the first one.
                             return reject ("Too many addresses found for: " + address);
                         }
                 }
